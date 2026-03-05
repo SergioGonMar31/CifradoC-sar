5ee6af62d78212e58f8d3631969506411b7233a1
Sergio <sergiogonzalezmarquinez@gmail.com>
2026-03-05 16:06:10 +0100

    Añadida funcion cifradoCesar

--- src/actividad/Principal.java
+++ src/actividad/Principal.java
@@ -1,10 +1,23 @@
 package actividad;
-
+import java.util.Scanner;
+import utiles.Depurar;
 public class Principal {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		String mensaje=Depurar.texto("Introduce el mensaje: ", sc);
+		int clave=Depurar.entero("Introduce la clave: ", sc);
 
+		String mensajeCifrado= cifradoCesar(mensaje, clave);
 	}
 
+	public static String cifradoCesar(String mensaje, int clave) {
+		String mensajeCifrado="";
+		
+		String abecedario="abcdefghijklmnñopqrstuvwxyz";
+		for(int i=0;i<abecedario.length();i++) {
+			
+		}
+		return mensajeCifrado;
+	}
 }
