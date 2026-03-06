3dc208aceb461c9e73dfeea7ff28088bdf804c42
Sergio <sergiogonzalezmarquinez@gmail.com>
2026-03-06 01:33:12 +0100

    Añadida funcion para cifrado.

--- src/actividad/Principal.java
+++ src/actividad/Principal.java
@@ -5,18 +5,29 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String mensaje=Depurar.texto("Introduce el mensaje: ", sc);
+		String mensaje=Depurar.texto("Introduce el mensaje: ", sc).toLowerCase();
 		int clave=Depurar.entero("Introduce la clave: ", sc);
 
 		String mensajeCifrado= cifradoCesar(mensaje, clave);
+		System.out.println("El mensaje cifrado es: "+mensajeCifrado);
 	}
 
 	public static String cifradoCesar(String mensaje, int clave) {
+		StringBuilder sb = new StringBuilder();
 		String mensajeCifrado="";
-		
+
 		String abecedario="abcdefghijklmnñopqrstuvwxyz";
-		for(int i=0;i<abecedario.length();i++) {
-			
+		for(int i=0;i<mensaje.length();i++) {
+			for(int j=0; j<abecedario.length();j++) {
+				if(mensaje.charAt(i)==abecedario.charAt(j)) {
+					int nuevaletra=j+clave;
+					if(nuevaletra>=abecedario.length()) {
+						nuevaletra=nuevaletra-abecedario.length();
+					}
+					sb.append(abecedario.charAt(nuevaletra));
+				}
+			}
+			mensajeCifrado=sb.toString();
 		}
 		return mensajeCifrado;
 	}
